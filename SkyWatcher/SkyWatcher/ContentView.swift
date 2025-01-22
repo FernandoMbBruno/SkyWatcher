f25f5481836771307995e82441a63152b6d0a786
FernandoBruno <fernandomatheusbruno@hotmail.com>
2025-01-21 23:38:58 -0300

    Initial commit

---
+++ SkyWatcher/SkyWatcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SkyWatcher
+//
+//  Created by Fernando Matheus Ferreira Adomaitis Bruno on 21/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SkyWatcher/SkyWatcher/SkyWatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyWatcherApp.swift
+//  SkyWatcher
+//
+//  Created by Fernando Matheus Ferreira Adomaitis Bruno on 21/01/25.
+//
+
+import SwiftUI
+
+@main
+struct SkyWatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
